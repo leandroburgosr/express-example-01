71fbcb62a12f150aeebe66efc3a8af80bf28f1c9
Leandro Burgos <leandroburgos@upeu.edu.pe>
2023-04-11 18:43:49 -0500

    add /api in routes, test POST method

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
+const express = require('express');
+
 const productsRouter = require('./produts.router');
 
 function routerApi(app) {
+  const router = express.Router();
+  app.use("/api", router);
   app.use('/products', productsRouter)
 
 }
